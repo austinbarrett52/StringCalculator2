9a943584817fef2079b56f461da846caeb2055e8
Austin Barrett <austinbarrett52@gmail.com>
2018-01-30 21:51:12 -0500

    seventh commit - phase 4, test class

--- Test/StringCalcTest.java
+++ Test/StringCalcTest.java
@@ -17,5 +17,10 @@ class StringCalcTest {
         assertEquals(-14, StringCalc.Add("-6,-8"));
         assertEquals(2, StringCalc.Add("8,-6"));
         assertEquals(0, StringCalc.Add("0,0"));
+        //test of many different lengths
+        assertEquals(15, StringCalc.Add("1,2,3,4,5"));
+        assertEquals(-15, StringCalc.Add("-1,-2,-3,-4,-5"));
+        assertEquals(22, StringCalc.Add("2,1,9,10"));
+        assertEquals(7, StringCalc.Add("1000,-1000,7"));
     }
 }
